69eee80b8871bd80ee6fd760d0796dbea3770e09
imdad Adelabou <imdadadelabou0@gmail.com>
2022-03-19 22:59:48 +0100

    Create findPeopleByName function to find a people by his name

--- myApp.js
+++ myApp.js
@@ -18,7 +18,9 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-    done(null /*, data*/ );
+    Person.find({ name: personName }, (err, data) => {
+        done(err, data);
+    });
 };
 
 const findOneByFood = (food, done) => {
